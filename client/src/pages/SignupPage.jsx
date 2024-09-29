bb86e8621a17cbfbf87d71f8af2267a1d53297b8
abhi-develope <prajapatiabhishek320@gmail.com>
2024-09-29 17:01:34 +0530

    created authstore and implement signup functionallity

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import Input from "../components/Input";
-import { Lock, Mail, User } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Loader, Lock, Mail, User } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
 import PasswordStrengthMeter from "../components/PasswordStrengthMeter";
+import { useAuthStore } from "../store/authStore";
 
 const SignupPage = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSignup = (e) => {
+  const navigate = useNavigate();
+
+  const {signup, error, isLoading} = useAuthStore();
+
+  const handleSignup = async (e) => {
     e.preventDefault();
-    console.log("signup");
+    
+    try {
+      await signup(email, password, name );
+      navigate("/verify-email");
+    } catch (error) {
+      console.log(error);
+      
+      
+    }
   };
   return (
     <motion.div
@@ -48,13 +61,15 @@ const SignupPage = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             />
+            {error && <p className="text-red-500 font-semibold mt-2">{error}</p>}
            <PasswordStrengthMeter password={password}/>
 
             <motion.button className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none fous:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
             whileHover={{scale: 1.02}}
             whileTap={{scale: 0.98}}
-            type="submit">
-                Sign Up
+            type="submit"
+            disabled={isLoading}>
+                {isLoading ? <Loader className="animate-spin mx-auto" size={24}/>: "Sign Up"}
                 
 
             </motion.button>
---
+++ client/src/store/authStore.js
@@ -0,0 +1,26 @@
+import {create} from "zustand";
+import axios from "axios";
+
+const API_URL = "http://localhost:3000/api/auth";
+
+axios.defaults.withCredentials = true;
+
+export const useAuthStore = create((set)=> ({
+    user:null,
+    isAuthenticated:false,
+    error:null,
+    isLoading:false,
+    isCheckingAuth:true,
+
+    signup: async (email, password, name) => {
+        set({isLoading: true, error: null});
+        try {
+            const response = await axios.post(`${API_URL}/signup`, {email, password, name});
+            set({user: response.data.user, isAuthenticated:true, isLoading: false});
+        } catch (error) {
+            set({ error: error.response.data.error || "Error signing up", isLoading: false});
+            throw error;
+            
+        }
+    }
+}))
