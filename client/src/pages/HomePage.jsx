2bf93c778be8645078794c9c6bdb5e803d72491c
abhi-develope <prajapatiabhishek320@gmail.com>
2024-10-01 05:44:44 +0530

    modified home page and create date.js

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { useAuthStore } from "../store/authStore";
+import { formatDate } from "../utils/date";
 
 const HomePage = () => {
+  const {user} = useAuthStore();
   return (
-    <div>
-      home
-    </div>
-  )
-}
+    <motion.div
+      initial={{ opacity: 0, scale: 0.9 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.9 }}
+      transition={{ duration: 0.5 }}
+      className="max-w-md w-full mx-auto mt-10 p-8 bg-gray-900 bg-opacity-80 backdrop-filter backdrop-blur-lg rounded-xl shadow-2xl border border-gray-800"
+    >
+      <div className="card card-side bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            alt="Movie"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{user.name}</h2>
+          <p>{user.email}</p>
+          <div className="card-actions justify-end">
+            
 
-export default HomePage
+
+          
+          </div>
+        </div>
+      </div>
+
+      <div className="card bg-base-100 w-96 shadow-xl">
+  
+  <div className="card-body">
+    <p>Account Activity</p>
+    <p>{formatDate(user.lastLogin)}</p>
+    <div className="card-actions justify-end">
+    </div>
+  </div>
+</div>
+
+
+      <motion.button
+					whileHover={{ scale: 1.05 }}
+					whileTap={{ scale: 0.95 }}
+					// onClick={handleLogout}
+					className='w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
+				font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700
+				 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900'
+				>
+					Logout
+				</motion.button>
+    </motion.div>
+  );
+};
+
+export default HomePage;
