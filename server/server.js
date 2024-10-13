5950fb330b89a030279065249bda17bb482d4296
abhi-develope <prajapatiabhishek320@gmail.com>
2024-10-13 15:16:26 +0530

    deployed configuration

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv"
 import cors from "cors"
+import path from "path"
 import cookieParser from "cookie-parser";
 import { connectDB } from "./db/connectDb.js";
 import authRoutes from "./routes/auth.route.js"
@@ -8,6 +9,7 @@ import authRoutes from "./routes/auth.route.js"
 dotenv.config();
 
 const app = express()
+const _dirname = path.resolve()
 
 const port = process.env.PORT || 5000
 
@@ -17,6 +19,14 @@ app.use(express.json());
 app.use(cookieParser()); // allow us to parse incoming cookies
 
 app.use("/api/auth", authRoutes)
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(_dirname, "/client/dist")));
+
+  app.get("*", (req, res)=> {
+    res.sendFile(path.resolve(_dirname, "client", "dist", "index.html"))
+  })
+}
 
 app.listen(port, () => {
     connectDB();
